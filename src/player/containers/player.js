b1a5acbc917f5fba439aff99a64a4cc8347c00fb
Jose <j.cisneros1195@gmail.com>
2019-03-05 01:19:34 -0600

    2_16-Controlando el buffer del video

--- src/player/containers/player.js
+++ src/player/containers/player.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 import Video from 'react-native-video';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ActivityIndicator } from 'react-native';
 import Layout from '../components/layout'
 
 class Player extends Component {
+  state = {
+    loading: true,
+  }
+  onBuffer = ({ isBuffering }) => {
+    this.setState({
+      loading: isBuffering,
+    })
+  }
+  onLoad = () => {
+    this.setState({
+      loading: false,
+    })
+  }
   render(){
     return(
 
@@ -15,12 +28,18 @@ class Player extends Component {
       //   >
       // </View>
       <Layout
+        loading={this.state.loading}
         video={
           <Video
             source={{uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'}}
             style={styles.video}
             resizeMode="contain"
+            onBuffer={this.onBuffer}
+            onLoad={this.onLoad}
           />
+        }
+        loader={
+          <ActivityIndicator color="red"/>
         }
         >
         </Layout>
