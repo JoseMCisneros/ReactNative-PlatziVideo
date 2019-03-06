60a7199e45e587a8d05463a22884e3c12add7bed
Jose <j.cisneros1195@gmail.com>
2019-03-05 21:30:09 -0600

    3_1-React Native + Redux

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import SuggestionList from './src/videos/containers/suggestion-list';
 import API from './utils/api';
 import CategoryList from './src/videos/containers/category-list';
 import Player from './src/player/containers/player';
+import { Provider } from 'react-redux';
+import store from './store';
 
 
 type Props = {};
@@ -27,18 +29,22 @@ export default class App extends Component<Props> {
   }
   render() {
     return (
-      <Home>
-        <Header />
-        <Player />
-        <Text>buscador</Text>
-        <Text>categorias</Text>
-        <CategoryList
-          list={this.state.categoryList}
-        />
-        <SuggestionList
-          list={this.state.suggestionList}
-        />
-      </Home>
+      <Provider
+        store={store}
+      >
+        <Home>
+          <Header />
+          <Player />
+          <Text>buscador</Text>
+          <Text>categorias</Text>
+          <CategoryList
+            list={this.state.categoryList}
+          />
+          <SuggestionList
+            list={this.state.suggestionList}
+          />
+        </Home>
+      </Provider>
     )
   }
 }
