dbbf3c5f60912e86e167fe2846f0f2457b7d9aa1
Jose <j.cisneros1195@gmail.com>
2019-03-05 22:05:30 -0600

    3_2-Crear los reducers

--- src/videos/containers/suggestion-list.js
+++ src/videos/containers/suggestion-list.js
@@ -4,6 +4,13 @@ import Layout from '../components/suggestion-list-layout';
 import Empty from '../components/empty';
 import Separator from '../components/vertical-separator';
 import Suggestion from '../components/suggestion';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+  return{
+    list: state.suggestionList
+  }
+}
 
 class SuggestionList extends Component {
   keyExtractor = item => item.id.toString()
@@ -29,4 +36,4 @@ class SuggestionList extends Component {
   }
 }
 
-export default SuggestionList;
+export default connect(mapStateToProps)(SuggestionList);
